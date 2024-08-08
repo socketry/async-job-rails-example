854edbbdce8aa2f9ce4855a4ddb9fff56027f123
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2024-08-08 15:38:38 +1200

    Updates/fixes.

--- config/initializers/async_job.rb
+++ config/initializers/async_job.rb
@@ -1,11 +1,13 @@
 
 require 'async/job'
+require 'async/job/backend/aggregate'
 require 'async/job/backend/redis'
 require 'async/job/backend/inline'
 
 Rails.application.configure do
 	config.async_job.backend_for "default" do
 		queue Async::Job::Backend::Redis
+		queue Async::Job::Backend::Aggregate
 	end
 	
 	config.async_job.backend_for "local" do
--- job-server.rb
+++ job-server.rb
@@ -1,7 +1,7 @@
 #!/usr/bin/env async-service
 
-require 'async/job/adapter/active_job/service'
+require 'async/job/adapter/active_job/environment'
 
 service "job-server" do
-	include Async::Job::Adapter::ActiveJob::Service
+	include Async::Job::Adapter::ActiveJob::Environment
 end
