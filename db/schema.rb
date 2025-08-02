88302093a7c2e56cf7a6346ff93f03ff39ba1de0
Samuel Williams <samuel.williams@shopify.com>
2025-08-02 12:24:00 +1200

    Updates + styles.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_25_111326) do
+ActiveRecord::Schema[8.0].define(version: 2024_02_25_111326) do
   create_table "job_executions", force: :cascade do |t|
     t.string "name"
     t.json "data"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end
